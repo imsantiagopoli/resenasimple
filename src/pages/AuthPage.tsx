53983e7d7fc0172e6d364a64860ae1a8ed267792
imsantiagopoli <96354983+imsantiagopoli@users.noreply.github.com>
2025-09-27 13:36:09 -0300

    Updated AuthPage.tsx

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1067,9 +1067,7 @@ const AuthPage: React.FC = () => {
                 {/* Header del paso opcional */}
                 <div className="text-center">
                   
-                  <p className="text-sm" style={{ color: 'rgb(107, 114, 128)' }}>
-                    Opcional: Agrega tus redes para mostrarlas en la página de votación
-                  </p>
+                  
                 </div>
 
                 <form onSubmit={handleStep3Submit} className="space-y-6">
