bc6c4f76e456a6752945934ce1c1452b5f854461
imsantiagopoli <96354983+imsantiagopoli@users.noreply.github.com>
2025-10-02 17:30:35 -0300

    Updated Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -32,7 +32,6 @@ const Sidebar: React.FC<SidebarProps> = ({ activePage }) => {
     { icon: QrCode, label: 'QR', id: 'pagina-qr', route: '/app/qr', color: 'rgb(75, 85, 99)' },
     { icon: Star, label: 'Reseñas', id: 'resenas', route: '/app/resenas', color: 'rgb(75, 85, 99)' },
     { icon: MessageSquare, label: 'Respuestas', id: 'respuestas', route: '/app/respuestas', color: 'rgb(75, 85, 99)' },
-    { icon: Users, label: 'Clientes', id: 'clientes', route: '/app/clientes', color: 'rgb(75, 85, 99)' },
   ];
 
   const handleLogout = async () => {
