caaeb74dee4db7fe978ca96c3e06d9d04bdbaf71
imsantiagopoli <96354983+imsantiagopoli@users.noreply.github.com>
2025-10-02 19:48:22 -0300

    Updated Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -30,8 +30,9 @@ const Sidebar: React.FC<SidebarProps> = ({ activePage }) => {
     { icon: Building2, label: 'Mi negocio', id: 'mi-negocio', route: '/app/mi-negocio', color: 'rgb(75, 85, 99)' },
     { icon: Vote, label: 'Página de votación', id: 'pagina-votacion', route: '/app/pagina-votacion', color: 'rgb(75, 85, 99)' },
     { icon: QrCode, label: 'QR', id: 'pagina-qr', route: '/app/qr', color: 'rgb(75, 85, 99)' },
-    { icon: Star, label: 'Reseñas', id: 'resenas', route: '/app/resenas', color: 'rgb(75, 85, 99)' },
+    // { icon: Star, label: 'Reseñas', id: 'resenas', route: '/app/resenas', color: 'rgb(75, 85, 99)' },
     { icon: MessageSquare, label: 'Respuestas', id: 'respuestas', route: '/app/respuestas', color: 'rgb(75, 85, 99)' },
+    // { icon: Users, label: 'Clientes', id: 'clientes', route: '/app/clientes', color: 'rgb(75, 85, 99)' },
   ];
 
   const handleLogout = async () => {
