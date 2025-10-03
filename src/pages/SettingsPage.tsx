3e1dea0cd154e26bdb7d488ad67a7a5f844b507b
imsantiagopoli <96354983+imsantiagopoli@users.noreply.github.com>
2025-10-02 22:03:27 -0300

    Updated SettingsPage.tsx

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -635,7 +635,7 @@ const SettingsPage: React.FC = () => {
 
       {/* Delete Account Modal */}
       {showDeleteModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 px-4">
           <div className="bg-white rounded-lg max-w-md w-full p-6">
             <div className="flex items-center space-x-3 mb-4">
               <div
